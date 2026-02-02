501b57e887aea24ccddfc74528c2d5ad54e80c12
Mayur Jadhav <mjmayurjadhav2@gmail.com>
2026-02-02 20:21:01 +0530

    Remove empty lines in sample.py

--- sample.py
+++ sample.py
@@ -1,19 +1,16 @@
 def add(a: int, b: int) -> int:
     return a + b
 
-
 def subtract(a: int, b: int) -> int:
     return a - b
 
 def multiply(a: int, b: int) -> int:
     return a * b
 
-
 def divide(a: int, b: int) -> float:
     if b == 0:
         raise ValueError("Division by zero is not allowed")
     return a / b
-
 
 if __name__ == "__main__":
     print("Add:", add(10, 5))
